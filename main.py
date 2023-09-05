e40210f5ce628410a067f923931332d2ffc9bdb0
Valdi <valdilaurentius@gmail.com>
2023-09-05 12:49:22 -0700

    apply the image modification steps in main.py

--- main.py
+++ main.py
@@ -14,3 +14,35 @@ from sklearn.metrics import classification_report
 
 CONTRASTER = ContrassBrightness()
 CLUSTERER = Clusterer()
+
+#used later as inputs to the neural network model
+proccessed_images = []
+image_labels = []
+
+#prepare image paths and randomizes them
+image_paths = list(paths.list_images("charts/ordered"))
+random.shuffle(image_paths)
+
+for imagePath in image_paths:
+    image = cv2.imread(imagePath)
+
+    #resize
+    image = imutils.resize(image, height=250)
+
+    #contrast (no change in brightness)
+    image = CONTRASTER.apply(image, 0, 60)
+
+    #blurring
+    image = cv2.medianBlur(image, 15)
+    image = cv2.GaussianBlur(image, (3,3), cv2.BORDER_DEFAULT)
+
+    #clustering
+    image = CLUSTERER.apply(image, 5)
+
+    # grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    #show the image
+    cv2.imshow("Gray", gray)
+    cv2.waitKey()
+    cv2.DestroyAllWindows()
